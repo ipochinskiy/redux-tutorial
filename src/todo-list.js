4d3472f8e761fbcb5c9432eb7fff5406a4be3ffe
Ilia Pochinskii <zorkiypnz@yandex.ru>
2016-11-14 21:39:25 +0100

    extracted Todo and TodoList components

--- src/todo-list.js
+++ src/todo-list.js
@@ -80,6 +80,35 @@ const FilterLink = ({
     );
 };
 
+const Todo = ({
+    onClick,
+    completed,
+    text,
+}) => (
+    <li
+        onClick={onClick}
+        style={{
+            textDecoration: completed ? 'line-through' : 'none',
+        }}>
+        {text}
+    </li>
+);
+
+const TodoList = ({
+    todos,
+    onTodoClick,
+}) => (
+    <ul>
+        {todos.map(todo =>
+            <Todo
+                key={todo.id}
+                {...todo}
+                onClick={() => onTodoClick(todo.id)}
+            >
+        )}
+    </ul>
+);
+
 const getVisibleTodos = (
     todos,
     filter,
@@ -126,24 +155,14 @@ class TodoApp extends Component {
                 }}>
                     Add todo
                 </button>
-                <ul>
-                    {visibleTodos.map(todo =>
-                        <li key={todo.id}
-                            onClick={() => {
-                                store.dispatch({
-                                    type: 'TOGGLE_TODO',
-                                    id: todo.id,
-                                });
-                            }}
-                            style={{
-                                textDecoration:
-                                    todo.completed ?
-                                        'line-through' :
-                                        'none',
-                            }}>
-                            {todo.text}
-                        </li>
-                    )}
+                <TodoList
+                    todos={visibleTodos}
+                    onTodoClick={(id) => {
+                        store.dispatch({
+                            type: 'TOGGLE_TODO',
+                            id,
+                        });
+                    }} />
                 </ul>
                 <p>
                     Show:
