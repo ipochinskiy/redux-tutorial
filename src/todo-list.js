b93125b103fc89e155443c08a381bad90512a62b
Ilia Pochinskii <zorkiypnz@yandex.ru>
2016-11-16 00:37:58 +0100

    added a persisted state

--- src/todo-list.js
+++ src/todo-list.js
@@ -197,8 +197,18 @@ const TodoApp = () => (
     </div>
 );
 
+const persistedState = {
+    todos: [{
+        id: '0',
+        text: 'Welcome back!',
+        completed: false,
+    }],
+};
+
+const store = createStore(todoApp, persistedState);
+
 ReactDOM.render(
-    <Provider store={createStore(todoApp)}>
+    <Provider store={store}>
         <TodoApp />
     </Provider>,
     document.getElementById('root')
