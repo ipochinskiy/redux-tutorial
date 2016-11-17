acd145a3bf9ec3ab5647b5c12c56a28de97bbdff
Ilia Pochinskii <zorkiypnz@yandex.ru>
2016-11-17 23:36:28 +0100

    refactored the entry point (extracted configureStore and a Root component)

--- src/index.js
+++ src/index.js
@@ -2,28 +2,13 @@ import 'babel-polyfill';
 
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 
-import throttle from 'lodash/throttle';
+import Root from './components/Root';
+import configureStore from './configureStore';
 
-import todoApp from './reducers';
-import App from './components/App';
-
-import { loadState, saveState } from './localStorage';
-
-const persistedState = loadState();
-const store = createStore(todoApp, persistedState);
-
-store.subscribe(throttle(() => {
-    saveState({
-        todos: store.getState().todos,
-    });
-}, 1000));
+const store = configureStore();
 
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <Root store={store} />,
   document.getElementById('root')
 );
