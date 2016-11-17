8f95dfc360335202c26746c166374edca5e18042
Ilia Pochinskii <zorkiypnz@yandex.ru>
2016-11-17 23:28:06 +0100

    added loading state from and saving it to localStorage, replaced global todoId with node-uuid

--- src/index.js
+++ src/index.js
@@ -8,14 +8,16 @@ import { createStore } from 'redux';
 import todoApp from './reducers';
 import App from './components/App';
 
-const persistedState = {
-  todos: [{
-    id: '0',
-    text: 'Welcome back!',
-    completed: false,
-  }],
-};
+import { loadState, saveState } from './localStorage';
+
+const persistedState = loadState();
 const store = createStore(todoApp, persistedState);
+
+store.subscribe(() => {
+    saveState({
+        todos: store.getState().todos,
+    });
+});
 
 render(
   <Provider store={store}>
