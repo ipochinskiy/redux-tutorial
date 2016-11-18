3104013748a1804e129f5e2126ea18dce21a47b9
Ilia Pochinskii <zorkiypnz@yandex.ru>
2016-11-18 01:05:21 +0100

    now handle visibility filter by a URL parameter

--- devServer.js
+++ devServer.js
@@ -15,7 +15,7 @@ app.use(webpackDevMiddleware(compiler, {
   publicPath: config.output.publicPath,
 }));
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
