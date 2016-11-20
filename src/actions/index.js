94c8da8a59e98a8228d6dd859aa68603ee1a1970
Ilia Pochinskii <zorkiypnz@yandex.ru>
2016-11-20 23:21:46 +0100

    replaced "manual" toggling isFetching flag with thunks

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ import { v4 } from 'node-uuid';
 
 import * as api from '../api';
 
-export const requestTodos = (filter) => ({
+const requestTodos = (filter) => ({
     type: 'REQUEST_TODOS',
     filter,
 });
@@ -13,10 +13,12 @@ const receiveTodos = (filter, response) => ({
     response,
 });
 
-export const fetchTodos = (filter) =>
-    api.fetchTodos(filter).then(response =>
-        receiveTodos(filter, response)
-    );
+export const fetchTodos = (filter) => (dispatch) => {
+    dispatch(requestTodos(filter));
+    return api.fetchTodos(filter).then(response => {
+        return dispatch(receiveTodos(filter, response));
+    });
+};
 
 export const addTodo = (text) => ({
   type: 'ADD_TODO',
